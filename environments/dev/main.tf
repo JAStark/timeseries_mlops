c835c02c5ca4b78f8d82112e943c83f75057fdb5
foxling-ee <foxling-ee@MacBook-Pro-3.local>
2022-12-11 23:07:42 +0000

    comment out cf code in tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -22,32 +22,32 @@ resource "google_storage_bucket" "timeseries_mlops_cloud_functions" {
   location = var.region
 }
 
-# Set up path to zip file containing code for this Cloud Function
-resource "google_storage_bucket_object" "historical_weather_cloud_function" {
-  name = "historical_weather.zip"
-  bucket = google_storage_bucket.timeseries_mlops_cloud_functions.name
-  source = "./cloud_functions"
-}
-
-# Set up the Cloud Function itself
-resource "google_cloudfunctions_function" "collect_historical_weather" {
-  name                  = "timeseries_mlops_collect_historical_weather"
-  description           = "Function to collect yesterday's hourly weather data"
-  runtime               = "python310"
-  available_memory_mb   = 128
-  timeout               = 120
-  source_archive_bucket = google_storage_bucket.timeseries_mlops_cloud_functions.name
-  source_archive_object = google_storage_bucket_object.historical_weather_cloud_function.name
-  entry_point           = "hello_fetch_historical_data"
-  # ingress_settings      = "ALLOW_INTERNAL_ONLY"
-  event_trigger {
-    event_type = "providers/cloud.pubsub/eventTypes/topic.publish"
-    resource = google_pubsub_topic.historical_weather_topic.id
-    failure_policy {
-      retry = false
-    }
-  }
-  environment_variables = {
-  PROJECT_ID  = var.project_id
-}
-}
+# # Set up path to zip file containing code for this Cloud Function
+# resource "google_storage_bucket_object" "historical_weather_cloud_function" {
+#   name = "historical_weather.zip"
+#   bucket = google_storage_bucket.timeseries_mlops_cloud_functions.name
+#   source = "./cloud_functions"
+# }
+#
+# # Set up the Cloud Function itself
+# resource "google_cloudfunctions_function" "collect_historical_weather" {
+#   name                  = "timeseries_mlops_collect_historical_weather"
+#   description           = "Function to collect yesterday's hourly weather data"
+#   runtime               = "python310"
+#   available_memory_mb   = 128
+#   timeout               = 120
+#   source_archive_bucket = google_storage_bucket.timeseries_mlops_cloud_functions.name
+#   source_archive_object = google_storage_bucket_object.historical_weather_cloud_function.name
+#   entry_point           = "hello_fetch_historical_data"
+#   # ingress_settings      = "ALLOW_INTERNAL_ONLY"
+#   event_trigger {
+#     event_type = "providers/cloud.pubsub/eventTypes/topic.publish"
+#     resource = google_pubsub_topic.historical_weather_topic.id
+#     failure_policy {
+#       retry = false
+#     }
+#   }
+#   environment_variables = {
+#   PROJECT_ID  = var.project_id
+# }
+# }
